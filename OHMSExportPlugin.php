e2d48bf66bf0f081005807cd7d91071f3de27686
Eric C. Weig <eweig@email.uky.edu>
2018-12-18 12:35:10 -0500

    Update OHMSExportPlugin.php

--- OHMSExportPlugin.php
+++ OHMSExportPlugin.php
@@ -5,9 +5,9 @@ class OHMSExportPlugin extends Omeka_Plugin_AbstractPlugin
 {
 
   //Add filters
-  protected $_filters = array(
-    'admin_navigation_main'
-  );
+//  protected $_filters = array(
+//    'admin_navigation_main'
+//  );
 
   // Define Hooks
   protected $_hooks = array(
@@ -38,15 +38,15 @@ class OHMSExportPlugin extends Omeka_Plugin_AbstractPlugin
   }
 
 
-  public function filterAdminNavigationMain($nav)
-  {
+//  public function filterAdminNavigationMain($nav)
+//  {
 
-    $nav[] = array(
-      'label' => __('OHMS Export'),
-      'uri' => url('/ohms-export/index')
-    );
-    return $nav;
-  }
+//    $nav[] = array(
+//      'label' => __('OHMS Export'),
+//      'uri' => url('/ohms-export/index')
+//    );
+//    return $nav;
+//  }
 
   // Adds a button to the admin search page for OHMS export
   public function hookAdminItemsBrowse($items)
@@ -72,19 +72,40 @@ class OHMSExportPlugin extends Omeka_Plugin_AbstractPlugin
   {
       $item = get_current_record('item');
       $itemId = $item->id;
+      
+    // get interview restriction field 
+    $restricted = metadata($item, array("Dublin Core", "Rights"));
+    $restricted = strip_formatting($restricted);
+    $chkressubstr = "Restricted";
+      
+    // get suppression field 
+    $suppressed = metadata($item, array("Suppression", "Suppressed -Suppress description"));
+    $suppressed = strip_formatting($suppressed);
+    $supsubstr = preg_replace('/\s+/', '', $suppressed);
+    $chksupsubstr = "Description suppressed: Yes";
+    $chksupsubstr = preg_replace('/\s+/', '', $chksupsubstr);
+      
+      
       $collection = get_collection_for_item();
       $collectionId = metadata($collection, 'id');
-      echo "<div class=\"panel\"><h4>Export OHMS XML</h4><p><a class='button blue' style='width:100%;' href='" . url('ohms-export/export/ohms?search=&advanced%5B0%5D%5Bjoiner%5D=and&advanced%5B0%5D%5Belement_id%5D=&advanced%5B0%5D%5Btype%5D=&advanced%5B0%5D%5Bterms%5D=&range=' . $itemId . '&collection=&type=&user=&tags=&public=&featured=&submit_search=Search+for+items&hits=0') . "'><input style='background-color:transparent;color:white;border:none;' type='button' value='Export Item as OHMS XML' /></a></p><p><a class='button blue' style='width:100%;' href='" . url('ohms-export/export/ohms?search=&advanced%5B0%5D%5Bjoiner%5D=and&advanced%5B0%5D%5Belement_id%5D=&advanced%5B0%5D%5Btype%5D=&advanced%5B0%5D%5Bterms%5D=&range=&collection=' . $collectionId . '&type=&user=&tags=&public=&featured=&submit_search=Search+for+items&hits=0') . "'><input style='background-color:transparent;color:white;border:none;' type='button' value='Export Collection as OHMS XML' /></a></p></div>";
+      
+      if (strpos($supsubstr, $chksupsubstr) !== false) {
+          echo "<div class=\"panel\"><h4>Export OHMS XML</h4><p><a class='button blue' style='width:100%;' href='" . url('ohms-export/export/ohms?search=&advanced%5B0%5D%5Bjoiner%5D=and&advanced%5B0%5D%5Belement_id%5D=232&advanced%5B0%5D%5Btype%5D=matches&advanced%5B0%5D%5Bterms%5D=False&advanced%5B0%5D%5Belement_id%5D=47&advanced%5B0%5D%5Btype%5D=is+exactly&advanced%5B0%5D%5Bterms%5D=Unrestricted&range=&collection=' . $collectionId . '&type=&user=&tags=&public=&featured=&submit_search=Search+for+items&hits=0') . "'><input style='background-color:transparent;color:white;border:none;' type='button' value='Export Collection as OHMS XML' /></a></p></div>"; 
+      } elseif (strpos($restricted, $chkressubstr) !== false) {
+                    echo "<div class=\"panel\"><h4>Export OHMS XML</h4><p><a class='button blue' style='width:100%;' href='" . url('ohms-export/export/ohms?search=&advanced%5B0%5D%5Bjoiner%5D=and&advanced%5B0%5D%5Belement_id%5D=232&advanced%5B0%5D%5Btype%5D=matches&advanced%5B0%5D%5Bterms%5D=False&advanced%5B0%5D%5Belement_id%5D=47&advanced%5B0%5D%5Btype%5D=is+exactly&advanced%5B0%5D%5Bterms%5D=Unrestricted&range=&collection=' . $collectionId . '&type=&user=&tags=&public=&featured=&submit_search=Search+for+items&hits=0') . "'><input style='background-color:transparent;color:white;border:none;' type='button' value='Export Collection as OHMS XML' /></a></p></div>";
+          } else {
+      echo "<div class=\"panel\"><h4>Export OHMS XML</h4><p><a class='button blue' style='width:100%;' href='" . url('ohms-export/export/ohms?search=&advanced%5B0%5D%5Bjoiner%5D=and&advanced%5B0%5D%5Belement_id%5D=&advanced%5B0%5D%5Btype%5D=&advanced%5B0%5D%5Bterms%5D=&range=' . $itemId . '&collection=&type=&user=&tags=&public=&featured=&submit_search=Search+for+items&hits=0') . "'><input style='background-color:transparent;color:white;border:none;' type='button' value='Export Item as OHMS XML' /></a></p><p><a class='button blue' style='width:100%;' href='" . url('ohms-export/export/ohms?search=&advanced%5B0%5D%5Bjoiner%5D=and&advanced%5B0%5D%5Belement_id%5D=232&advanced%5B0%5D%5Btype%5D=matches&advanced%5B0%5D%5Bterms%5D=False&advanced%5B0%5D%5Belement_id%5D=47&advanced%5B0%5D%5Btype%5D=is+exactly&advanced%5B0%5D%5Bterms%5D=Unrestricted&range=&collection=' . $collectionId . '&type=&user=&tags=&public=&featured=&submit_search=Search+for+items&hits=0') . "'><input style='background-color:transparent;color:white;border:none;' type='button' value='Export Collection as OHMS XML' /></a></p></div>";  }
   }
   
   public function hookAdminCollectionsShowSidebar()
   {
             $collectionRec = get_current_record('collection');
             $collectionID = $collectionRec->id;
-            echo "<div class=\"panel\"><h4>Export OHMS XML</h4><p><a class='button blue' style='width:100%;' href='" . url('ohms-export/export/ohms?search=&advanced%5B0%5D%5Bjoiner%5D=and&advanced%5B0%5D%5Belement_id%5D=&advanced%5B0%5D%5Btype%5D=&advanced%5B0%5D%5Bterms%5D=&range=&collection=' . $collectionID . '&type=&user=&tags=&public=&featured=&submit_search=Search+for+items&hits=0') . "'><input style='background-color:transparent;color:white;border:none;' type='button' value='Export Collection as OHMS XML' /></a></p></div>";
+echo "<div class=\"panel\"><h4>Export OHMS XML</h4><p><a class='button blue' style='width:100%;' href='" . url('ohms-export/export/ohms?search=&advanced%5B0%5D%5Bjoiner%5D=and&advanced%5B0%5D%5Belement_id%5D=232&advanced%5B0%5D%5Btype%5D=matches&advanced%5B0%5D%5Bterms%5D=False&advanced%5B0%5D%5Belement_id%5D=47&advanced%5B0%5D%5Btype%5D=is+exactly&advanced%5B0%5D%5Bterms%5D=Unrestricted&range=&collection=' . $collectionID . '&type=&user=&tags=&public=&featured=&submit_search=Search+for+items&hits=0') . "'><input style='background-color:transparent;color:white;border:none;' type='button' value='Export Collection as OHMS XML' /></a></p></div>";
   }
   
 
 }
 
 ?>
+
